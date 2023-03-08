e3314c6e9c336178459a78abee4599fb4c66564b
PolinaSmolina <polina.smolina.00@bk.ru>
2023-03-08 21:07:31 +0700

    Initial project Add testtask

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Lesson1; 
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Hallo11111");
+        Console.Write(" world");
+        /*Console.WriteLine("Hi");
+        string text = "I am Polly" это текстовая информация
+        int value = Int32.MaxValue; это целочисленное значение, 2 в степени 32
+        double doubleValue = Double.MaxValue; это число с плавающей точкой, тут покажет самое большое число с точкой, которое может отобразить
+        bool result = value > doubleValue
+        char symbol = 'A'
+        это всё те типы данных, которые будем юзать
+        */
+        /*по арифметическим действиям
+        int value1 = 58
+        int value2 = 14
+        int valueResult = value1 / value2; результат будет целочисленный
+        % - остаток от деления, когда надо оставить четные и нечетные числа
+        == - это сравнение на равенство
+        != - это сравнение на неравенство
+        || - или
+        && - и
+         */
+    }
+}
+
